2079dd999056896930e0008f8ce69ab961a0ba4c
Anubhav Kumar Srivastava <mitram.email75@gmail.com>
2026-04-19 12:00:58 +0530

    Add solution:1984_maximum-distance-between-a-pair-of-values_Medium

---
+++ maximum-distance-between-a-pair-of-values.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxDistance(int[] nums1, int[] nums2) {
+    	int n = nums1.length;
+    	int m = nums2.length;
+    	int i = 0;
+    	int j = 0;
+    	int max = 0;
+        while(i < n && j < m){
+        	if(nums1[i] <= nums2[j]){
+        		max = Math.max(max, j - i);
+        		j++;
+        	}
+        	else{
+        		i++;
+        	}
+        }
+        return max;
+    }
+}
+// @lc code=end
